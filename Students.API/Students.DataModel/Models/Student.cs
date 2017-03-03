555c594553631fe9ef18e55ac5fbdc3949a9614e
Luis Rogerio <lurogerio@prestadores.amil.com.br>
2017-03-03 17:01:57 -0300

    Criando estrutura.

---
+++ Students.API/Students.DataModel/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace Students.DataModel.Models
+{
+    public class Student
+    {
+        [BsonElement("_id")]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Class { get; set; }
+    }
+}
---
+++ Students.API/Students.DataModel/StudentRepository/StudentRepository.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Driver;
+using Students.DataModel.Models;
+
+namespace Students.DataModel.StudentRepository
+{
+    public class StudentRepository<T> where T : class
+    {
+        private IMongoDatabase _database;
+
+        private string _tableName;
+
+        private IMongoCollection<T> _collection;
+
+        public StudentRepository(IMongoDatabase db, string tblName)
+        {
+            _database = db;
+            _tableName = tblName;
+            _collection = _database.GetCollection<T>(tblName);
+        }
+
+        //public T Get(int i)
+        //{
+            
+        //}
+    }
+}
